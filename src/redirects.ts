c666d401172b8a7967e27e2ed087086b19acec59
Carlton Joseph <carlton.joseph@gmail.com>
2020-03-13 06:46:17 +0000

    fix redirect script bugs

--- src/redirects.ts
+++ src/redirects.ts
@@ -7,12 +7,13 @@ const sites = [
 ];
 
 const srcDir = "./redirects";
-const dstDir = "./mnt/volume_sfo2_02/cj/";
+const dstDir = "/mnt/volume_sfo2_02/cj/";
+const fileName = "index.html";
 
 (async () => {
   sites.map(site => {
-    const src = path.join(srcDir, site);
-    const dst = path.join(dstDir, site);
+    const src = path.join(srcDir, site, fileName);
+    const dst = path.join(dstDir, site, fileName);
     fs.copyFileSync(src,dst);
   });
 })().catch(e => console.log(e));
